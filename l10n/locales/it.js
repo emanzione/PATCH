cb29ccef0628a41eab883534be2eb0703bdbf579
Emanuele Manzione <m4nu.91@gmail.com>
2018-11-26 14:04:34 +0000

    Added Italian localization.

---
+++ l10n/locales/it.js
@@ -0,0 +1,74 @@
+var localeIT = {
+	"Connecting...": "Connessione...",
+	"Retrieving required data...": "Ricercando le informazioni necessarie...",
+	"Connection with the P.A.T.C.H. Admin Tool has been lost!": "La connessione con l'Admin Tool di P.A.T.C.H. è stata interrotta!",
+	"Your browser does NOT support WebSocket. Update it in order to use P.A.T.C.H.!": "Il tuo browser non supporta WebSocket. Aggiornalo per utilizzare P.A.T.C.H.!",
+
+	/* HINTS */
+	"You can write a review on Asset Store to help other customers!": "Puoi scrivere una review sull'Asset Store per aiutare gli altri clienti!",
+	"Doc contains a lot of useful information! Read it, make me proud of you!": "La documentazione contiene molte informazioni utili! Leggila e rendimi orgoglioso!",
+	"Did I already say you should read the doc one more time? :)": "Ho già detto che dovresti leggere la documentazione ancora una volta? :)",
+	"You can join Discord channel to chat with us!": "Puoi unirti al nostro canale Discord per chattare con noi!",
+	"You can send me a mail to ask for help! But read the doc first :)": "Puoi mandarmi una mail se hai difficoltà! Ma assicurati di aver letto la documentazione prima! :)",
+	"You can use P.A.T.C.H. not only with Unity! UE, CE, MonoGame, etc: P.A.T.C.H. works with everything!": "Puoi usare P.A.T.C.H. non soltanto con Unity! UE, CE, MonoGame, ecc: P.A.T.C.H. funziona con tutto!",
+	"You can sell your own P.A.T.C.H. skins and plugins on Asset Store!": "Puoi realizzare e vendere le tue grafiche e i tuoi plugin per P.A.T.C.H. sull'Asset Store!",
+	"You must set .NET 2.0 or higher as your API Compatibility Level!": "Assicurati di impostare l'API Compatibility Level almeno a .NET 2.0!",
+	"It is a good practice to run P.A.T.C.H. as admin!": "E' una buona pratica avviare P.A.T.C.H. come amministratore!",
+	"You must not run your Launcher in the Editor!": "Non avviare il Launcher dall'Editor!",
+	/* _____ */
+
+	/* HINTS WINDOW */
+	"Did you know?": "Lo sapevi?",
+	"Next": "Succ.",
+	/* _____ */
+
+	/* TOPBAR */
+	"Doc": "Doc",
+	"Tutorials": "Tutorials",
+	"Roadmap": "Roadmap",
+	"Issues": "Issues",
+	/* _____ */
+
+	/* FOOTER */
+	"Version": "Versione",
+	"About": "About",
+	"All rights reserved.": "Tutti i diritti riservati.",
+	/* _____ */
+
+	/* SIDEBAR */
+	"Dashboard": "Dashboard",
+	"Project": "Progetto",
+	"Builds": "Build",
+	"Patches": "Patch",
+	"Launcher": "Launcher",
+	"Server": "Server",
+	"Options": "Opzioni",
+	"Info": "Info",
+	/* _____ */
+
+	/* REVIEW ADVICE */
+	"Leave a review!": "Lascia una recensione!",
+	"Hey! If you love P.A.T.C.H. help us! Leave a review on Asset Store!": "Hey! Se sei soddisfatto di P.A.T.C.H. che ne diresti di aiutarci a crescere? Lascia una recensione positiva sull'Asset Store!",
+	"Let's do it!": "Facciamolo!",
+	"Remind me later": "Più tardi",
+	/* _____ */
+
+	/* NEW PROJECT */
+	"New project": "Nuovo progetto",
+	"No project has been found. Create a new one!": "Nessun progetto trovato. Creane uno nuovo!",
+	"Create": "Crea",
+	/* _____ */
+
+	/* PLATFORM MANAGEMENT */
+	"Platforms management": "Gestione piattaforme",
+	/* _____ */
+	
+	/* BRANCH MANAGEMENT */
+	"Branches management": "Gestione branch",
+	/* _____ */
+
+	/* OPTIONS */
+	"Language": "Lingua",
+	"Set the language for this tool.": "Imposta la lingua per questo tool.",
+	/* _____ */
+};
