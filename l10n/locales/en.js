eac3a96cc85b53c962cca3f50e9dc7c96acd6a11
Emanuele Manzione <m4nu.91@gmail.com>
2018-12-11 13:31:32 +0000

    Modified localization keys to be language-agnostic and added the "en" localization file.

---
+++ l10n/locales/en.js
@@ -0,0 +1,75 @@
+var localeEN = {
+	"[connection_connecting]": "Connecting...",
+	"[connection_connecting]": "Connected!",
+	"[connection_retrieving_data]": "Retrieving required data...",
+	"[connection_connection_lost]": "Connection with the P.A.T.C.H. Admin Tool has been lost! Retrying...",
+	"[connection_websocket_is_not_supported]": "Your browser does NOT support WebSocket. Update it in order to use P.A.T.C.H.!",
+
+	/* HINTS */
+	"[hints_write_a_review]": "You can write a review on Asset Store to help other customers!",
+	"[hints_read_the_doc_1]": "Doc contains a lot of useful information! Read it, make me proud of you!",
+	"[hints_read_the_doc_2]": "Did I already say you should read the doc one more time? :)",
+	"[hints_join_discord]": "You can join Discord channel to chat with us!",
+	"[hints_send_me_a_mail]": "You can send me a mail to ask for help! But read the doc first :)",
+	"[hints_it_works_with_everything]": "You can use P.A.T.C.H. not only with Unity! UE, CE, MonoGame, etc: P.A.T.C.H. works with everything!",
+	"[hints_sell_your_own]": "You can sell your own P.A.T.C.H. skins and plugins on Asset Store!",
+	"[hints_dotnet_compatibility]": "You must set .NET 2.0 or higher as your API Compatibility Level!",
+	"[hints_run_as_admin]": "It is a good practice to run P.A.T.C.H. as admin!",
+	"[hints_do_not_run_in_editor]": "You must not run your Launcher in the Editor!",
+	/* _____ */
+
+	/* HINTS WINDOW */
+	"[hints_title]": "Did you know?",
+	"[hints_next_button]": "Next",
+	/* _____ */
+
+	/* TOPBAR */
+	"[topbar_doc]": "Doc",
+	"[topbar_tutorials]": "Tutorials",
+	"[topbar_roadmap]": "Roadmap",
+	"[topbar_issues]": "Issues",
+	/* _____ */
+
+	/* FOOTER */
+	"[footer_version]": "Version",
+	"[footer_about]": "About",
+	"[footer_rights_reserved]": "All rights reserved.",
+	/* _____ */
+
+	/* SIDEBAR */
+	"[sidebar_dashboard]": "Dashboard",
+	"[sidebar_project]": "Project",
+	"[sidebar_builds]": "Builds",
+	"[sidebar_patches]": "Patches",
+	"[sidebar_launcher]": "Launcher",
+	"[sidebar_server]": "Server",
+	"[sidebar_options]": "Options",
+	"[sidebar_info]": "Info",
+	/* _____ */
+
+	/* REVIEW ADVICE */
+	"[leave_a_review_title]": "Leave a review!",
+	"[leave_a_rewiew_body]": "Hey! If you love P.A.T.C.H. help us! Leave a review on Asset Store!",
+	"[leave_a_review_do_it]": "Let's do it!",
+	"[leave_a_review_remind_me_later]": "Remind me later",
+	/* _____ */
+
+	/* NEW PROJECT */
+	"[new_project_title]": "New project",
+	"[new_project_body_no_project_found]": "No project has been found. Create a new one!",
+	"[new_project_create_button]": "Create",
+	/* _____ */
+
+	/* PLATFORM MANAGEMENT */
+	"[platforms_management_title]": "Platforms management",
+	/* _____ */
+	
+	/* BRANCH MANAGEMENT */
+	"[branches_management_title]": "Branches management",
+	/* _____ */
+
+	/* OPTIONS */
+	"[options_language_section_title]": "Language",
+	"[options_language_section_label]": "Set the language for this tool.",
+	/* _____ */
+};
--- l10n/locales/it.js
+++ l10n/locales/it.js
@@ -1,74 +1,75 @@
 var localeIT = {
-	"Connecting...": "Connessione...",
-	"Retrieving required data...": "Ricercando le informazioni necessarie...",
-	"Connection with the P.A.T.C.H. Admin Tool has been lost!": "La connessione con l'Admin Tool di P.A.T.C.H. è stata interrotta!",
-	"Your browser does NOT support WebSocket. Update it in order to use P.A.T.C.H.!": "Il tuo browser non supporta WebSocket. Aggiornalo per utilizzare P.A.T.C.H.!",
+	"[connection_connecting]": "Connessione...",
+	"[connection_connecting]": "Connesso!",
+	"[connection_retrieving_data]": "Ricercando le informazioni necessarie...",
+	"[connection_connection_lost]": "La connessione con l'Admin Tool di P.A.T.C.H. è stata interrotta! Riconnessione...",
+	"[connection_websocket_is_not_supported]": "Il tuo browser non supporta WebSocket. Aggiornalo per utilizzare P.A.T.C.H.!",
 
 	/* HINTS */
-	"You can write a review on Asset Store to help other customers!": "Puoi scrivere una review sull'Asset Store per aiutare gli altri clienti!",
-	"Doc contains a lot of useful information! Read it, make me proud of you!": "La documentazione contiene molte informazioni utili! Leggila e rendimi orgoglioso!",
-	"Did I already say you should read the doc one more time? :)": "Ho già detto che dovresti leggere la documentazione ancora una volta? :)",
-	"You can join Discord channel to chat with us!": "Puoi unirti al nostro canale Discord per chattare con noi!",
-	"You can send me a mail to ask for help! But read the doc first :)": "Puoi mandarmi una mail se hai difficoltà! Ma assicurati di aver letto la documentazione prima! :)",
-	"You can use P.A.T.C.H. not only with Unity! UE, CE, MonoGame, etc: P.A.T.C.H. works with everything!": "Puoi usare P.A.T.C.H. non soltanto con Unity! UE, CE, MonoGame, ecc: P.A.T.C.H. funziona con tutto!",
-	"You can sell your own P.A.T.C.H. skins and plugins on Asset Store!": "Puoi realizzare e vendere le tue grafiche e i tuoi plugin per P.A.T.C.H. sull'Asset Store!",
-	"You must set .NET 2.0 or higher as your API Compatibility Level!": "Assicurati di impostare l'API Compatibility Level almeno a .NET 2.0!",
-	"It is a good practice to run P.A.T.C.H. as admin!": "E' una buona pratica avviare P.A.T.C.H. come amministratore!",
-	"You must not run your Launcher in the Editor!": "Non avviare il Launcher dall'Editor!",
+	"[hints_write_a_review]": "Puoi scrivere una review sull'Asset Store per aiutare gli altri clienti!",
+	"[hints_read_the_doc_1]": "La documentazione contiene molte informazioni utili! Leggila e rendimi orgoglioso!",
+	"[hints_read_the_doc_2]": "Ho già detto che dovresti leggere la documentazione ancora una volta? :)",
+	"[hints_join_discord]": "Puoi unirti al nostro canale Discord per chattare con noi!",
+	"[hints_send_me_a_mail]": "Puoi mandarmi una mail se hai difficoltà! Ma assicurati di aver letto la documentazione prima! :)",
+	"[hints_it_works_with_everything]": "Puoi usare P.A.T.C.H. non soltanto con Unity! UE, CE, MonoGame, ecc: P.A.T.C.H. funziona con tutto!",
+	"[hints_sell_your_own]": "Puoi realizzare e vendere le tue grafiche e i tuoi plugin per P.A.T.C.H. sull'Asset Store!",
+	"[hints_dotnet_compatibility]": "Assicurati di impostare l'API Compatibility Level almeno a .NET 2.0!",
+	"[hints_run_as_admin]": "E' una buona pratica avviare P.A.T.C.H. come amministratore!",
+	"[hints_do_not_run_in_editor]": "Non avviare il Launcher dall'Editor!",
 	/* _____ */
 
 	/* HINTS WINDOW */
-	"Did you know?": "Lo sapevi?",
-	"Next": "Succ.",
+	"[hints_title]": "Lo sapevi?",
+	"[hints_next_button]": "Succ.",
 	/* _____ */
 
 	/* TOPBAR */
-	"Doc": "Doc",
-	"Tutorials": "Tutorials",
-	"Roadmap": "Roadmap",
-	"Issues": "Issues",
+	"[topbar_doc]": "Doc",
+	"[topbar_tutorials]": "Tutorials",
+	"[topbar_roadmap]": "Roadmap",
+	"[topbar_issues]": "Issues",
 	/* _____ */
 
 	/* FOOTER */
-	"Version": "Versione",
-	"About": "About",
-	"All rights reserved.": "Tutti i diritti riservati.",
+	"[footer_version]": "Versione",
+	"[footer_about]": "About",
+	"[footer_rights_reserved]": "Tutti i diritti riservati.",
 	/* _____ */
 
 	/* SIDEBAR */
-	"Dashboard": "Dashboard",
-	"Project": "Progetto",
-	"Builds": "Build",
-	"Patches": "Patch",
-	"Launcher": "Launcher",
-	"Server": "Server",
-	"Options": "Opzioni",
-	"Info": "Info",
+	"[sidebar_dashboard]": "Dashboard",
+	"[sidebar_project]": "Progetto",
+	"[sidebar_builds]": "Build",
+	"[sidebar_patches]": "Patch",
+	"[sidebar_launcher]": "Launcher",
+	"[sidebar_server]": "Server",
+	"[sidebar_options]": "Opzioni",
+	"[sidebar_info]": "Info",
 	/* _____ */
 
 	/* REVIEW ADVICE */
-	"Leave a review!": "Lascia una recensione!",
-	"Hey! If you love P.A.T.C.H. help us! Leave a review on Asset Store!": "Hey! Se sei soddisfatto di P.A.T.C.H. che ne diresti di aiutarci a crescere? Lascia una recensione positiva sull'Asset Store!",
-	"Let's do it!": "Facciamolo!",
-	"Remind me later": "Più tardi",
+	"[leave_a_review_title]": "Lascia una recensione!",
+	"[leave_a_rewiew_body]": "Hey! Se sei soddisfatto di P.A.T.C.H. che ne diresti di aiutarci a crescere? Lascia una recensione positiva sull'Asset Store!",
+	"[leave_a_review_do_it]": "Facciamolo!",
+	"[leave_a_review_remind_me_later]": "Più tardi",
 	/* _____ */
 
 	/* NEW PROJECT */
-	"New project": "Nuovo progetto",
-	"No project has been found. Create a new one!": "Nessun progetto trovato. Creane uno nuovo!",
-	"Create": "Crea",
+	"[new_project_title]": "Nuovo progetto",
+	"[new_project_body_no_project_found]": "Nessun progetto trovato. Creane uno nuovo!",
+	"[new_project_create_button]": "Crea",
 	/* _____ */
 
 	/* PLATFORM MANAGEMENT */
-	"Platforms management": "Gestione piattaforme",
+	"[platforms_management_title]": "Gestione piattaforme",
 	/* _____ */
 	
 	/* BRANCH MANAGEMENT */
-	"Branches management": "Gestione branch",
+	"[branches_management_title]": "Gestione branch",
 	/* _____ */
 
 	/* OPTIONS */
-	"Language": "Lingua",
-	"Set the language for this tool.": "Imposta la lingua per questo tool.",
+	"[options_language_section_title]": "Lingua",
+	"[options_language_section_label]": "Imposta la lingua per questo tool.",
 	/* _____ */
 };
